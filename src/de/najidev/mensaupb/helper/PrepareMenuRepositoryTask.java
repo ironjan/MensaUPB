2b0d2eaf978a9d77f00086f8c40d6634cf2ccd03
Jan Lippert <lippertsjan@gmail.com>
2013-10-18 13:11:23 +0200

    Added some logging

--- src/de/najidev/mensaupb/helper/PrepareMenuRepositoryTask.java
+++ src/de/najidev/mensaupb/helper/PrepareMenuRepositoryTask.java
@@ -22,52 +22,47 @@ import org.xml.sax.InputSource;
 import de.najidev.mensaupb.activities.MainActivity;
 import de.najidev.mensaupb.entity.Menu;
 import de.najidev.mensaupb.entity.MenuRepository;
-
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.os.AsyncTask;
+import android.util.*;
 
-public class PrepareMenuRepositoryTask extends AsyncTask<Void, Void, Void>
-{
+public class PrepareMenuRepositoryTask extends AsyncTask<Void, Void, Void> {
+	private static final String TAG = PrepareMenuRepositoryTask.class.getSimpleName();
 	MainActivity activity;
 	Context applicationContext;
 	MenuRepository menuRepository;
 	ProgressDialog dialog;
 
 	protected final String charsetOriginal = "windows-1252";
-	protected final String charsetWanted   = "utf-8";
+	protected final String charsetWanted = "utf-8";
 
-	public PrepareMenuRepositoryTask(final MainActivity activity,
-			Context applicationContext,
-			MenuRepository menuRepository)
-	{
-		this.activity           = activity;
+	public PrepareMenuRepositoryTask(final MainActivity activity, Context applicationContext,
+			MenuRepository menuRepository) {
+		this.activity = activity;
 		this.applicationContext = applicationContext;
-		this.menuRepository     = menuRepository;
+		this.menuRepository = menuRepository;
 
-		this.dialog             = new ProgressDialog(activity);
+		this.dialog = new ProgressDialog(activity);
 		this.dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
 		this.dialog.setMessage("Download des Mensaplans...");
 
 		this.dialog.setOnCancelListener(new OnCancelListener() {
 
-			public void onCancel(DialogInterface dialog)
-			{
+			public void onCancel(DialogInterface dialog) {
 				activity.finish();
 			}
 		});
 	}
 
 	@Override
-	protected void onPreExecute()
-	{
+	protected void onPreExecute() {
 		this.dialog.show();
 	}
 
 	@Override
-	protected void onPostExecute(Void result)
-	{
+	protected void onPostExecute(Void result) {
 		super.onPostExecute(result);
 
 		this.activity.getDayPagerAdapter().notifyDataSetChanged();
@@ -77,21 +72,20 @@ public class PrepareMenuRepositoryTask extends AsyncTask<Void, Void, Void>
 	}
 
 	@Override
-	protected Void doInBackground(Void... params)
-	{
-		// a list, where we will save all results
+	protected Void doInBackground(Void... params) {
 		List<Menu> menus = new ArrayList<Menu>();
-		List<Menu> tmp;
 
-		for (String location : this.applicationContext.getAvailableLocations().values())
-		{
-			tmp = this.parseXML(this.downloadFile("http://www.studentenwerk-pb.de/fileadmin/xml/" + location + ".xml"));
+		for (String location : this.applicationContext.getAvailableLocations().values()) {
+			String url = "http://www.studentenwerk-pb.de/fileadmin/xml/" + location + ".xml";
+			InputSource downloadedXmlContent = downloadFile(url);
+			List<Menu> parsedMenus = parseXML(downloadedXmlContent);
 
-			for (Menu menu : tmp)
+			for (Menu menu : parsedMenus) {
 				menu.setLocation(location);
+			}
 
 			// add to menu list
-			menus.addAll(tmp);
+			menus.addAll(parsedMenus);
 
 		}
 
@@ -101,69 +95,55 @@ public class PrepareMenuRepositoryTask extends AsyncTask<Void, Void, Void>
 		return null;
 	}
 
-	public InputSource downloadFile(String url)
-	{
-		try
-		{
+	public InputSource downloadFile(String url) {
+		try {
+			Log.v(TAG, "Starting download of " + url);
+
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpResponse response = httpclient.execute(new HttpGet(url));
 
-			BufferedReader br = new BufferedReader(
-					new InputStreamReader(response.getEntity().getContent(), charsetOriginal)
-					);
+			BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity()
+					.getContent(), charsetOriginal));
 			StringBuilder sb = new StringBuilder();
 
 			String l = br.readLine().toLowerCase().replace(charsetOriginal, charsetWanted);
 			sb.append(l + "\n");
-			while ((l = br.readLine()) != null)
+			while ((l = br.readLine()) != null) {
 				sb.append(l);
+			}
 
 			br.close();
+			Log.v(TAG, "Download of " + url + " complete");
 			return new InputSource(new StringReader(sb.toString()));
-		}
-		catch (Exception ignored)
-		{
+		} catch (Exception ignored) {
 			return null;
 		}
 	}
 
-	protected List<Menu> parseXML(InputSource is)
-	{
+	protected List<Menu> parseXML(InputSource is) {
 		List<Menu> list = new ArrayList<Menu>();
 		// parse file
-		try
-		{
-			Document document = DocumentBuilderFactory
-					.newInstance()
-					.newDocumentBuilder()
-					.parse(is);
+		try {
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
 
 			NodeList dayList = document.getElementsByTagName("tag");
 
-			for (int i = 0; i < dayList.getLength(); i++)
-			{
+			for (int i = 0; i < dayList.getLength(); i++) {
 				Date date = null;
-				for (int k = 0; k < dayList.item(i).getChildNodes().getLength(); k++)
-				{
+				for (int k = 0; k < dayList.item(i).getChildNodes().getLength(); k++) {
 					Node node = dayList.item(i).getChildNodes().item(k);
-					if (node.getNodeName().equals("datum"))
-					{
-						date = new SimpleDateFormat("dd.MM.yyyy").parse(node.getFirstChild().getNodeValue());
-					}					
-					else if (node.getNodeName().equals("menue"))
-					{
+					if (node.getNodeName().equals("datum")) {
+						date = new SimpleDateFormat("dd.MM.yyyy").parse(node.getFirstChild()
+								.getNodeValue());
+					} else if (node.getNodeName().equals("menue")) {
 						NodeList menuDetails = node.getChildNodes();
 						Menu menu = new Menu();
 						menu.setDate(date);
 
-						for (int j = 0; j < menuDetails.getLength(); j++)
-						{
-							String name  = menuDetails.item(j).getNodeName();
-							String value = menuDetails.item(j)
-									.getFirstChild()
-									.getNodeValue()
-									.replaceAll("\\(.+\\)( \\*)?$", "")
-									.trim();
+						for (int j = 0; j < menuDetails.getLength(); j++) {
+							String name = menuDetails.item(j).getNodeName();
+							String value = menuDetails.item(j).getFirstChild().getNodeValue()
+									.replaceAll("\\(.+\\)( \\*)?$", "").trim();
 
 							if (name.equals("menu"))
 								menu.setTitle(value);
@@ -182,8 +162,9 @@ public class PrepareMenuRepositoryTask extends AsyncTask<Void, Void, Void>
 			}
 		}
 		// if an error occurs, we can't do anything against it...
-		catch (Exception e) { }
+		catch (Exception e) {
+		}
 
-		return list;		
+		return list;
 	}
 }
