dc143e8f31ad88eb0847ac2d5d3b459c4620e0a3
Jan Lippert <lippertsjan@gmail.com>
2014-02-01 15:52:42 +0100

    (Slow) location switching

--- src/de/najidev/mensaupb/activities/Test.java
+++ src/de/najidev/mensaupb/activities/Test.java
@@ -4,6 +4,8 @@ import android.os.*;
 import android.support.v4.app.*;
 import android.support.v4.view.*;
 import android.support.v7.app.*;
+import android.text.*;
+import android.widget.*;
 
 import org.androidannotations.annotations.*;
 
@@ -15,27 +17,57 @@ import de.najidev.mensaupb.fragments.*;
 import de.najidev.mensaupb.stw.*;
 
 @EActivity(R.layout.activity_menu_listing)
-public class Test extends ActionBarActivity {
+public class Test extends ActionBarActivity implements ActionBar.OnNavigationListener {
 
     public static final int WEEKEND_OFFSET = 2;
     @ViewById(R.id.pager)
     ViewPager mViewPager;
     private DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
     private static final SimpleDateFormat SDF = Menu.DATABASE_DATE_FORMAT;
+    public static final String[] RESTAURANTS = new String[]{"Mensa", "Abendmensa", "Gownsmen's Pub", "HNF (Hotspot)"};
+    private String mLocation = "Mensa";
+    private DemoCollectionPagerAdapter[] adapters = new DemoCollectionPagerAdapter[4];
 
 
     @AfterViews
     void showMenus() {
+        initActionBar();
         initPager();
+    }
+
+    private void initActionBar() {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+        // Specify a SpinnerAdapter to populate the dropdown list.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(actionBar.getThemedContext(),
+                android.R.layout.simple_spinner_item, android.R.id.text1,
+                RESTAURANTS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        actionBar.setListNavigationCallbacks(adapter, this);
     }
 
     private void initPager() {
         // ViewPager and its adapters use support library
         // fragments, so use getSupportFragmentManager.
+        switchPagerAdapter(0);
+    }
+
+    private void switchPagerAdapter(int i) {
         mDemoCollectionPagerAdapter =
-                new DemoCollectionPagerAdapter(
-                        getSupportFragmentManager());
+                getPagerAdapter(i);
         mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+
+    private DemoCollectionPagerAdapter getPagerAdapter(int i) {
+        if (adapters[i] == null) {
+            adapters[i] =
+                    new DemoCollectionPagerAdapter(
+                            getSupportFragmentManager(), RESTAURANTS[i]);
+        }
+        return adapters[i];
     }
 
     private String getNextWeekDayAsString(int i) {
@@ -55,12 +87,26 @@ public class Test extends ActionBarActivity {
         return cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
     }
 
+    @Override
+    public boolean onNavigationItemSelected(int i, long l) {
+        if (TextUtils.equals(mLocation, RESTAURANTS[i]))
+            return true;
+
+        mLocation = RESTAURANTS[i];
+
+        switchPagerAdapter(i);
+
+        return true;
+    }
+
     public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
 
         Fragment[] fragments = new Fragment[3];
+        String mLocation;
 
-        public DemoCollectionPagerAdapter(FragmentManager fm) {
+        public DemoCollectionPagerAdapter(FragmentManager fm, String location) {
             super(fm);
+            mLocation = location;
         }
 
         @Override
@@ -83,7 +129,7 @@ public class Test extends ActionBarActivity {
             Bundle arguments = new Bundle();
 
             arguments.putString(MenuListingFragment.ARG_DATE, getNextWeekDayAsString(i));
-            arguments.putString(MenuListingFragment.ARG_LOCATION, "Mensa");
+            arguments.putString(MenuListingFragment.ARG_LOCATION, mLocation);
 
             fragment.setArguments(arguments);
             return fragment;
@@ -98,6 +144,11 @@ public class Test extends ActionBarActivity {
         public CharSequence getPageTitle(int position) {
             return getNextWeekDayAsString(position);
         }
+    }
+
+
+    private String getLocation() {
+        return mLocation;
     }
 
     private Date getNextWeekDay(int offset) {
