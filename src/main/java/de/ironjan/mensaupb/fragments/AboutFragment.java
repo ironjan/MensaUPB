8d4490dcc7891b90eb2cc531fe5f906f5b541a38
Jan Lippert <ljan@mail.upb.de>
2014-04-10 21:43:24 +0200

    Added open source notes
    * open source notes in about
    * link to my project page
    * removed old "own" icon

--- src/main/java/de/ironjan/mensaupb/fragments/AboutFragment.java
+++ src/main/java/de/ironjan/mensaupb/fragments/AboutFragment.java
@@ -1,6 +1,7 @@
 package de.ironjan.mensaupb.fragments;
 
 import android.support.v4.app.*;
+import android.widget.*;
 
 import org.androidannotations.annotations.*;
 
@@ -8,5 +9,7 @@ import de.ironjan.mensaupb.*;
 @EFragment(R.layout.fragment_about)
 public class AboutFragment extends Fragment {
 
-
+@ViewById(R.id.txtDependencies)
+    @FromHtml(R.string.dependencies)
+    TextView txtDependencies;
 }
