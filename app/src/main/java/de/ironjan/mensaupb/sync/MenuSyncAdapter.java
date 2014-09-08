20a69d41abd51a29d40cd614edd66b9a76a238b3
Jan Lippert <lippertsjan@gmail.com>
2014-09-08 19:17:13 +0200

    Persisting menus now

--- app/src/main/java/de/ironjan/mensaupb/sync/MenuSyncAdapter.java
+++ app/src/main/java/de/ironjan/mensaupb/sync/MenuSyncAdapter.java
@@ -3,20 +3,28 @@ package de.ironjan.mensaupb.sync;
 import android.accounts.*;
 import android.annotation.*;
 import android.content.*;
+import android.database.*;
+import android.database.sqlite.*;
 import android.os.*;
+import android.provider.*;
 import android.text.*;
 
+import com.j256.ormlite.android.*;
+import com.j256.ormlite.dao.*;
 import com.j256.ormlite.stmt.query.*;
+import com.j256.ormlite.support.*;
 
 import org.slf4j.*;
 
 import java.io.*;
 import java.net.*;
+import java.sql.*;
 import java.text.*;
 import java.util.*;
 
 import de.ironjan.mensaupb.*;
 import de.ironjan.mensaupb.library.stw.*;
+import de.ironjan.mensaupb.persistence.*;
 
 /**
  * SyncAdapter to download and persist menus.
@@ -34,14 +42,14 @@ public class MenuSyncAdapter extends AbstractThreadedSyncAdapter {
     private MenuSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
         mContentResolver = context.getContentResolver();
-        stwRest = new StwRest_(context) ;
+        stwRest = new StwRest_(context);
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private MenuSyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
         mContentResolver = context.getContentResolver();
-        stwRest = new StwRest_(context) ;
+        stwRest = new StwRest_(context);
     }
 
     public static MenuSyncAdapter getInstance(Context context) {
@@ -69,11 +77,28 @@ public class MenuSyncAdapter extends AbstractThreadedSyncAdapter {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("onPerformeSync({},{},{},{},{})", new Object[]{account, bundle, s, contentProviderClient, syncResult});
         }
+        DatabaseManager databaseManager = new DatabaseManager();
+        DatabaseHelper helper = (databaseManager.getHelper(getContext()));
+        ConnectionSource connectionSource =
+                new AndroidConnectionSource(helper);
 
-        for(RawMenu rawMenu: stwRest.getAll()){
-            // TODO: persist update
+        try {
+            Dao<RawMenu, ?> dao = DaoManager.createDao(connectionSource, RawMenu.class);
+
+            for (RawMenu rawMenu : stwRest.getAll()) {
+                LOGGER.warn("{}",rawMenu    );
+                List<RawMenu> local = dao.queryForEq(RawMenu.PSEUDO_HASH, rawMenu.getPseudoHash());
+                if(local.size() > 0){
+                    rawMenu.set_id(local.get(0).get_id());
+                }
+                dao.createIfNotExists(rawMenu);
+            }
+
+            databaseManager.releaseHelper(helper);
+        } catch (java.sql.SQLException e) {
+            LOGGER.warn("onPerformeSync({},{},{},{},{}) failed because of exception", new Object[]{account, bundle, s, contentProviderClient, syncResult});
+            LOGGER.error(e.getMessage(), e);
         }
-
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("onPerformeSync({},{},{},{},{}) done", new Object[]{account, bundle, s, contentProviderClient, syncResult});
         }
