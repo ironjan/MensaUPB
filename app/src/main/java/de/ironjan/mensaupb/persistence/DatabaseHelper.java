746c6fd1d9ab0da6ea52e8cada5f7cdaa0c2ed16
Jan Lippert <lippertsjan@gmail.com>
2014-10-26 18:51:23 +0100

    Fixed crash?

--- app/src/main/java/de/ironjan/mensaupb/persistence/DatabaseHelper.java
+++ app/src/main/java/de/ironjan/mensaupb/persistence/DatabaseHelper.java
@@ -21,7 +21,7 @@ import de.ironjan.mensaupb.sync.*;
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "mensaupb.db";
 
-    private static final int DATABASE_VERSION = 9;
+    private static final int DATABASE_VERSION = 10;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class.getSimpleName());
     private final Context mContext;
@@ -44,24 +44,26 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
             LOGGER.error("Can't create database", e);
             throw new RuntimeException(e);
         }
+        requestSync();
         if (LOGGER.isDebugEnabled()) LOGGER.debug("onCreate() done");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int old, int newVersion) {
         if (LOGGER.isInfoEnabled()) LOGGER.info("onUpgrade()");
-        if (old <= 8) {
-            dropDatabase();
-            onCreate(sqLiteDatabase, connectionSource);
+        try {
+            if (old <= 9) {
+                TableUtils.dropTable(connectionSource, RawMenu.class, true);
+                onCreate(sqLiteDatabase, connectionSource);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Can't update database", e);
+            throw new RuntimeException(e);
         }
         requestSync();
         if (LOGGER.isDebugEnabled()) LOGGER.debug("onUpgrade() done");
     }
 
-    private void dropDatabase() {
-        mContext.deleteDatabase(DATABASE_NAME);
-        if (LOGGER.isInfoEnabled()) LOGGER.info("Dropped database.");
-    }
 
     private void requestSync() {
         Bundle settingsBundle = new Bundle();
