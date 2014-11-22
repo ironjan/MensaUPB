c3226d60014091933953a9c8b3b4abc6109220e0
Jan Lippert <lippertsjan@gmail.com>
2014-11-22 13:53:12 +0100

    Displaying menu details via Activity

---
+++ app/src/main/java/de/ironjan/mensaupb/activities/MenuDetails.java
@@ -0,0 +1,42 @@
+package de.ironjan.mensaupb.activities;
+
+import android.annotation.*;
+import android.os.*;
+import android.support.v4.app.*;
+import android.support.v7.app.*;
+import android.widget.*;
+
+import org.androidannotations.annotations.*;
+
+import de.ironjan.mensaupb.*;
+import de.ironjan.mensaupb.fragments.*;
+
+/**
+ * Wrapping activity for {@link de.ironjan.mensaupb.fragments.AboutFragment}.
+ */
+@SuppressLint("Registered")
+@EActivity(R.layout.activity_menu_details)
+public class MenuDetails extends ActionBarActivity {
+    @Extra(value = MenuDetailFragment.ARG_ID)
+    long menuId;
+
+
+    @AfterViews
+    void bindFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fragmentMenuDetails = MenuDetailFragment.newInstance(menuId);
+        ft.replace(R.id.fragmentMenuDetails, fragmentMenuDetails, "fragmentMenuDetails");
+        ft.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @OptionsItem(android.R.id.home)
+    void navUp() {
+        NavUtils.navigateUpFromSameTask(this);
+    }
+}
