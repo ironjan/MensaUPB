62ea8c8841a2e9d9326ae65600de930a07418b79
Jan Lippert <lippertsjan@gmail.com>
2015-02-04 22:49:06 +0100

    Better naming

--- app/src/androidTest/java/de/ironjan/mensaupb/stw/filters/NameFilterTest.java
+++ app/src/androidTest/java/de/ironjan/mensaupb/stw/filters/NameFilterTest.java
@@ -18,23 +18,22 @@ public class NameFilterTest extends TestCase {
     NameFilter nameFilter = new NameFilter();
     RawMenu menu = new RawMenu();
 
-
-    public void testSetName_deNoClean() throws Exception {
+    public void test_deNoClean() throws Exception {
         menu.setName_de(NO_CLEAN_NECESSARY);
         assertEquals(NO_CLEAN_NECESSARY, nameFilter.filter(menu).getName_de());
     }
 
-    public void testSetName_deClean1Vert() throws Exception {
+    public void test_deClean1Vert() throws Exception {
         menu.setName_de(CLEAN_NECESSARY_1_VERT);
         assertEquals(CLEANED_1_VERT, nameFilter.filter(menu).getName_de());
     }
 
-    public void testSetName_deClean2Vert() throws Exception {
+    public void test_deClean2Vert() throws Exception {
         menu.setName_de(CLEAN_NECESSARY_2_VERT);
         assertEquals(CLEANED_2_VERT, nameFilter.filter(menu).getName_de());
     }
 
-    public void testSetName_deClean3Vert() throws Exception {
+    public void test_deClean3Vert() throws Exception {
         menu.setName_de(CLEAN_NECESSARY_3_VERT);
         assertEquals(CLEANED_3_VERT, nameFilter.filter(menu).getName_de());
     }
@@ -44,22 +43,22 @@ public class NameFilterTest extends TestCase {
         assertEquals(CLEANED_BREAK_FAST_BURGER, nameFilter.filter(menu).getName_de());
     }
 
-    public void testSetName_enNoClean() throws Exception {
+    public void test_enNoClean() throws Exception {
         menu.setName_en(NO_CLEAN_NECESSARY);
         assertEquals(NO_CLEAN_NECESSARY, nameFilter.filter(menu).getName_en());
     }
 
-    public void testSetName_enClean1Vert() throws Exception {
+    public void test_enClean1Vert() throws Exception {
         menu.setName_en(CLEAN_NECESSARY_1_VERT);
         assertEquals(CLEANED_1_VERT, nameFilter.filter(menu).getName_en());
     }
 
-    public void testSetName_enClean2Vert() throws Exception {
+    public void test_enClean2Vert() throws Exception {
         menu.setName_en(CLEAN_NECESSARY_2_VERT);
         assertEquals(CLEANED_2_VERT, nameFilter.filter(menu).getName_en());
     }
 
-    public void testSetName_enClean3Vert() throws Exception {
+    public void test_enClean3Vert() throws Exception {
         menu.setName_en(CLEAN_NECESSARY_3_VERT);
         assertEquals(CLEANED_3_VERT, nameFilter.filter(menu).getName_en());
     }
