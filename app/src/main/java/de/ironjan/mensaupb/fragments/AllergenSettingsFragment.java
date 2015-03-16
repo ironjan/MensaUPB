4ff11168e00e849135c29b6b8091ca1f12a6ebdc
Jan Lippert <lippertsjan@gmail.com>
2015-03-16 22:50:25 +0100

    Added non-functional Settings-Activity

---
+++ app/src/main/java/de/ironjan/mensaupb/fragments/AllergenSettingsFragment.java
@@ -0,0 +1,14 @@
+package de.ironjan.mensaupb.fragments;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import de.ironjan.mensaupb.R;
+
+/**
+ * Fragment to change filter settings w.r.t allergens and additionals.
+ */
+@EFragment(R.layout.fragment_allergen_settings)
+public class AllergenSettingsFragment extends Fragment {
+}
