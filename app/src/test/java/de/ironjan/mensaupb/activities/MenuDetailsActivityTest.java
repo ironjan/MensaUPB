54c0e5843a7efae33573c7580c40499178850997
Jan Lippert <lippertsjan@gmail.com>
2015-03-17 01:16:50 +0100

    Fixed Robolectric and added some tests
     * Switched to robolectric 3.0-SNAPSHOT
     * Testing linkifying of About TextView
     * Added some failing tests for other activities

---
+++ app/src/test/java/de/ironjan/mensaupb/activities/MenuDetailsActivityTest.java
@@ -0,0 +1,22 @@
+package de.ironjan.mensaupb.activities;
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * TODO start this activity
+ * TODO start other activities from this one
+ * TODO navigate up from others to this
+ * http://robolectric.org/activity-lifecycle/
+ */
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class MenuDetailsActivityTest {
+    @Test
+    public void test_FailNIY() {
+        Assert.fail("No tests implemented for this Activity yet.");
+    }
+}
