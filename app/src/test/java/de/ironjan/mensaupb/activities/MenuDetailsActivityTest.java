f17888f1dcdd219230830400f9b407ab3ba9db1b
Jan Lippert <lippertsjan@gmail.com>
2015-03-19 11:04:16 +0100

    Removed todos which were placed in the wrong class.

--- app/src/test/java/de/ironjan/mensaupb/activities/MenuDetailsActivityTest.java
+++ app/src/test/java/de/ironjan/mensaupb/activities/MenuDetailsActivityTest.java
@@ -6,12 +6,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-/**
- * TODO start this activity
- * TODO start other activities from this one
- * TODO navigate up from others to this
- * http://robolectric.org/activity-lifecycle/
- */
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class MenuDetailsActivityTest {
