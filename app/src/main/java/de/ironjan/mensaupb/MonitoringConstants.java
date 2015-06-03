f123ab33796e850d502095ec0cbebed840029e90
Jan Lippert <lippertsjan@gmail.com>
2015-06-03 10:24:23 +0200

    Tracking screen views for "/restaurant/day" instead of changes.

--- app/src/main/java/de/ironjan/mensaupb/MonitoringConstants.java
+++ app/src/main/java/de/ironjan/mensaupb/MonitoringConstants.java
@@ -1,13 +1,9 @@
 package de.ironjan.mensaupb;
 
 /**
- * Class to keep track of what should be monitored
+ * Class to keep track of what should be monitored in addition to screen views
  */
 public class MonitoringConstants {
-    public static final String CATEGORY_DAYS = "day-navigation";
     public static final String CATEGORY_MENUS = "menus";
-    public static final String CATEGORY_RESTAURANTS = "restaurant-navigation";
-
-    public static final String ACTION_CHANGE = "change";
     public static final String ACTION_SCROLLING = "scrolling";
 }
