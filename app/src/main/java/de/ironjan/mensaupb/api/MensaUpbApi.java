3578646c27b7a53d7486d5477d77034225e86715
Jan Lippert <lippertsjan@gmail.com>
2018-07-22 13:52:25 +0200

    Added more routes to API

--- app/src/main/java/de/ironjan/mensaupb/api/MensaUpbApi.java
+++ app/src/main/java/de/ironjan/mensaupb/api/MensaUpbApi.java
@@ -1,6 +1,7 @@
 package de.ironjan.mensaupb.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import de.ironjan.mensaupb.model.Menu;
 import de.ironjan.mensaupb.model.Restaurant;
@@ -12,6 +13,15 @@ public interface MensaUpbApi {
     @GET("restaurants")
     Observable<List<Restaurant>> getRestaurants();
 
+    @GET("restaurant")
+    Observable<List<Restaurant>> getRestaurant(@Query("key") String key);
+
     @GET("menus")
     Observable<List<Menu>> getMenus(@Query("restaurant") String restaurant, @Query("date") String date);
+
+    @GET("menu")
+    Observable<Optional<Menu>> getMenu(@Query("key") String key);
+
+    @GET("simple_menusa")
+    Observable<List<Menu>> getSimpleMenus(@Query("restaurant") String restaurant, @Query("date") String date);
 }
