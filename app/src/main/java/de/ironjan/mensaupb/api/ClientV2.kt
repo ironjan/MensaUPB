9de44fb97818c7ab18d83db57f694fee69a4701f
Jan Lippert <lippertsjan@gmail.com>
2018-11-02 01:09:03 +0100

    Extended client

--- app/src/main/java/de/ironjan/mensaupb/api/ClientV2.kt
+++ app/src/main/java/de/ironjan/mensaupb/api/ClientV2.kt
@@ -6,6 +6,7 @@ import de.ironjan.mensaupb.api.model.Menu
 interface ClientV2 {
     fun getMenus(): Either<String, Array<Menu>>
     fun getMenus(restaurant: String, date: String): Either<String, Array<Menu>>
+    fun getMenu(key: String): Either<String, Menu>
 
     companion object {
 
