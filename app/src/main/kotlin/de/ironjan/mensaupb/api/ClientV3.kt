40fb796536de4107a19783790ea283e09767589e
Jan Lippert <lippertsjan@gmail.com>
2019-07-18 18:37:14 +0200

    Removed client v2

--- app/src/main/kotlin/de/ironjan/mensaupb/api/ClientV3.kt
+++ app/src/main/kotlin/de/ironjan/mensaupb/api/ClientV3.kt
@@ -3,7 +3,10 @@ package de.ironjan.mensaupb.api
 import arrow.core.Either
 import de.ironjan.mensaupb.api.model.Menu
 
-interface ClientV3 : ClientV2{
+interface ClientV3 {
+    fun getMenus(): Either<String, Array<Menu>>
+    fun getMenus(restaurant: String, date: String): Either<String, Array<Menu>>
+    fun getMenu(key: String): Either<String, Menu>
     fun getMenus(noCache: Boolean): Either<String, Array<Menu>>
     fun getMenus(restaurant: String, date: String, noCache: Boolean): Either<String, Array<Menu>>
 
