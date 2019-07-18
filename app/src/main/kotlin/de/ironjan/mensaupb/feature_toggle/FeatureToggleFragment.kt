7477f29dc859d59e0f2bf72dab1b298909ad54a8
Jan Lippert <lippertsjan@gmail.com>
2019-07-18 18:39:52 +0200

    Removed feature toggle ui

--- app/src/main/kotlin/de/ironjan/mensaupb/feature_toggle/FeatureToggleFragment.kt
+++ app/src/main/kotlin/de/ironjan/mensaupb/feature_toggle/FeatureToggleFragment.kt
@@ -2,17 +2,13 @@ package de.ironjan.mensaupb.feature_toggle
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.widget.CheckBox
 import de.ironjan.mensaupb.R
 import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EFragment
-import org.androidannotations.annotations.ViewById
 
 @EFragment(R.layout.fragment_feature_toggles)
 open class FeatureToggleFragment : Fragment() {
 
-    @ViewById(R.id.toggleClientV3)
-    internal lateinit var toggleClientV3: CheckBox
 
     lateinit var featureTogglePrefs: FeatureTogglePrefs
 
@@ -25,11 +21,9 @@ open class FeatureToggleFragment : Fragment() {
 
     @AfterViews
     fun loadSettings() {
-        toggleClientV3.isChecked = featureTogglePrefs.clientV3Enabled
     }
 
     override fun onPause() {
-        featureTogglePrefs.clientV3Enabled = toggleClientV3.isChecked
         super.onPause()
     }
 }
